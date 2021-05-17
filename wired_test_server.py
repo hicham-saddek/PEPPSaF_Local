f726c58709be6eae3077c8b85cbc0b3042e8fb3e
Hicham SADDEK <h.saddek@serrahub.com>
2021-05-17 14:28:59 +0200

    Adding test servers

---
+++ wired_test_server.py
@@ -0,0 +1,28 @@
+import json
+
+from flask import Flask
+from flask_restful import reqparse, Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+request = reqparse.RequestParser()
+
+
+class CentralController(Resource):
+    @staticmethod
+    def get():
+        return "You can use post to post new data to the server"
+
+    @staticmethod
+    def post():
+        request.add_argument("data")
+        args = json.loads(request.parse_args())
+
+        return {"data-wireless": args["data"]} if "data" in args else {"error-wired": "Nothing received"}
+
+
+api.add_resource(CentralController, '/post-data')
+
+if __name__ == "__main__":
+    app.run(debug=False, host="node-01.central.peppsaf", port=5252)
