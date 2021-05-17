f560b59bfeaa05ba339f2f138fe8e4ca004cb541
Hicham SADDEK <h.saddek@serrahub.com>
2021-05-17 03:24:20 +0200

    Fixing bug, console and tools run is not detected

--- console.py
+++ console.py
@@ -4,11 +4,11 @@ from PEPPSaF.Application.Console import Application as Console
 from PEPPSaF.System.Tools.ToolApplication import Application as Tools
 
 if __name__ == "__main__":
-    if 1 in sys.argv and sys.argv[1] == "tools":
+    if len(sys.argv) > 1 and str(sys.argv[1]) == "tools":
         print("|- --- --- --- -| P E P P S a F |- --- --- --- -|")
         Application = Tools(sys.argv)
         exit("Exited with code: " + str(Application.run()))
-    elif 1 in sys.argv and  sys.argv[1] == "console":
+    elif len(sys.argv) > 1 and str(sys.argv[1]) == "console":
         print("|- --- --- --- -| P E P P S a F |- --- --- --- -|")
         Application = Console(sys.argv)
         exit("Exited with code: " + str(Application.run()))
