f2fced9802463f75be4a22bdbc5891434720ac5b
Hicham SADDEK <h.saddek@serrahub.com>
2021-05-17 02:29:50 +0200

    Allowing the console to run with one argument

--- console.py
+++ console.py
@@ -4,5 +4,6 @@ from PEPPSaF.Application.Console.ConsoleApplication import Application as Consol
 from PEPPSaF.Application.Tools.ToolApplication import Application as ToolApplication
 
 if __name__ == "__main__":
-    Application = ToolApplication(sys.argv) if len(sys.argv) > 1 else ConsoleApplication(sys.argv)
+    Application = ToolApplication(sys.argv) if len(sys.argv) > 2 else ConsoleApplication(sys.argv)
+    print("|- --- --- --- -| P E P P S a F |- --- --- --- -|")
     exit("Exited with code: " + str(Application.run()))
